b7d7985127ae54cef72d3a0fd05ba5aab3422b95
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-11-07 23:16:10 -0300

    Update usersRoute.js

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/register', UserController.registerUser); // Rota de registro de usuario
 router.post('/login', UserController.loginUser); // Rota de login de usuario
-router.post('/alterarCadastro', UserController.updateUser); // Rota de registro de usuario
+router.post('/alterarCadastro', UserController.updateUser); // Rota de atualização de usuario
 
 
 module.exports = router;
