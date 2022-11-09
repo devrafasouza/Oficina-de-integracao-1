812ad6607b3579fd38581430fd985cadcf2a2adf
Rafael de Souza Alcântara <64436420+devrafasouza@users.noreply.github.com>
2022-11-09 20:08:39 -0300

    Delete models (refactoring names)

--- src/routes/usersRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const UserController = require('../controllers/UserController');
-
-const router = Router();
-
-router.post('/register', UserController.registerUser); // Rota de registro de usuario
-
-module.exports = router;
