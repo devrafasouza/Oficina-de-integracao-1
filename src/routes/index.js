c8a7667489e916be8b697a372ee20494b54a1d41
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-11-11 02:25:54 -0300

    2

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,10 @@ const user = require('./userRoute'); /* rotas de usuarios */
 
 module.exports = app => {
   app.use(cors());
-  app.use(bodyParser.json());
+  app.use(bodyParser.json());  
   app.use(user); /* Chama as rotas de usuario para o app */
+  app.get('/' , (request,
+    response) => {
+      response.send('Agora foi...');
+    }); //Captura a tela inicial e responde 
 }
