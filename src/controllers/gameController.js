dbfc2bae94d458d0a5bb0a2e5d449e42840556ac
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-11-10 23:19:42 -0300

    1

---
+++ src/controllers/gameController.js
@@ -0,0 +1,41 @@
+const database = require('../models');
+
+
+class gameController {
+  static async addGame(req, res) {
+    const newGame = req.body;
+        try {
+        const newGame = await database.Games.create(newGame); /* cria um novo jogo no banco com o metodo create do sequelize */
+        return res.status(200).menssage("jogo Inserido com sucesso");
+    } catch (error) {
+        return res.status(500).menssage("Falha ao inserir Game");
+    }
+}
+
+static async editGame(req, res) {
+ /*Como alterar o jogo no banco? */
+    const newGame = req.body;   
+        try {
+        const newGame = await database.Games.create(newGame); /* cria um novo jogo no banco com o metodo create do sequelize */
+        return res.status(200).menssage("jogo Inserido com sucesso");
+    } catch (error) {
+        return res.status(500).menssage("Falha ao inserir Game");
+    }
+}
+
+static async deleteGame(req, res) {
+    const newGame = req.body;
+
+    /*Como receber o modal de confirmação? */
+        try {
+        const newGame = await database.Games.create(newGame); /* cria um novo jogo no banco com o metodo create do sequelize */
+        return res.status(200).menssage("jogo Inserido com sucesso");
+    } catch (error) {
+        return res.status(500).menssage("Falha ao inserir Game");
+    }
+}
+
+}
+
+module.exports = GameController;
+
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,5 +7,6 @@ router.post('/register', UserController.registerUser); // Rota de registro de us
 router.post('/login', /*UserController.loginUser*/); // Rota de login de usuario
 router.patch('/usuarios/:id', /*UserController.disableUser*/); // Rota para desativar um usuario
 router.get('/wallet/:id', /*UserController.walletUser */); // Rota para acessar a carteira do usuario
+router.post('/alterarCadastro', /*UserController.updateUser*/); // Rota de atualização de usuario
 
 module.exports = router;
