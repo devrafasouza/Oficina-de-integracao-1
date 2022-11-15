7dbe216d046b45b285ba72b078a2544e69cc29f1
Rafael de Souza Alcântara <64436420+devrafasouza@users.noreply.github.com>
2022-11-15 16:06:47 -0300

    Adding src/middlewares/auth.js
    middleware de autenticação

---
+++ src/middlewares/auth.js
@@ -0,0 +1,35 @@
+const jsonwebtoken = require('jsonwebtoken');
+const database = require('../models');
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const { id_user } = req.params
+
+    if(!authHeader) {
+      return res.status(404).json({ mensagem: "Falta de token"});
+      
+    }
+    
+    const [, token] = authHeader.split(" ")
+    
+
+    try {
+      const { sub } = jsonwebtoken.verify(token, "5f4dcc3b5aa765d61d8327deb882cf99");
+
+      const user = await database.User.findOne({
+        where: {
+          id_user: sub
+        }
+        
+      })
+      req.user = user;
+      req.id_user = id_user;
+      next();
+      if(!user) {
+        return res.status(404).json({ mensagem: "Usuario não existe na base de dados"});
+      }
+    } catch (error) {
+      return res.status(404).json({ mensagem: "Token invalido"});
+         
+    }
+}
