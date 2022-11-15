6fcc34f9d7f3af3a2bc660cb7e36d8e0190c29cf
Rafael de Souza Alcântara <64436420+devrafasouza@users.noreply.github.com>
2022-11-15 15:58:52 -0300

    Update src/routes/userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UserController');
+const auth = require('../middlewares/auth.js');
 
 const router = Router();
 
 router.post('/register', UserController.registerUser); // Rota de registro de usuario
-//router.post('/teste', UserController.testeUser); // Rota de login de usuario
-//router.patch('/usuarios/:id', UserController.disableUser); // Rota para desativar um usuario
+router.post('/login', UserController.loginUser); // Rota de login de usuario
+router.post('/wallet:id', auth, UserController.purchaseUser); // Rota para desativar um usuario
 //router.get('/wallet/:id', /*UserController.walletUser */); // Rota para acessar a carteira do usuario
 
 module.exports = router;
