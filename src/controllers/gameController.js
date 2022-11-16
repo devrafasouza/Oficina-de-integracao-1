b6ebd06e86073893db061c0d352df9f911c4eee7
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-11-16 00:30:24 -0300

    FindOneGame

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,7 +1,6 @@
 const { where } = require('sequelize');
 const database = require('../models');
-//const Op = Sequelize.Op
-
+const Op = require('sequelize');
 
 class GameController {
 
@@ -21,9 +20,10 @@ class GameController {
 static async updateGame(req, res) { 
     const updateGame = req.body;   
         try {
-        const updateGameItem = await database.Game.update(updateGame,{ where: {id_game:updateGame.id_game}});      
-        
-        return res.status(200).json(updateGameItem);
+        const updateGameItem = await database.Game.update(updateGame,{ 
+            where: {id_game:updateGame.id_game
+            }});      
+              return res.status(200).json(updateGameItem);
     } catch (error) {
         return res.status(500).json(error.message);//"Falha ao atualizar Game");
     }
@@ -33,29 +33,57 @@ static async updateGame(req, res) {
 static async deleteGame(req, res) {
     const deleteGame = req.body;    
         try {
-        const deleteGameItem = await database.Game.destroy({where:{id_game: deleteGame.id_game}});      
-        
+        const deleteGameItem = await database.Game.destroy({
+            where:{id_game: deleteGame.id_game}});     
         return res.status(200).json(deleteGameItem);
     } catch (error) {
         return res.status(500).json(error.message);//"Falha ao deletar Game");
     }
 }
-
-}
-/*
+//Encontrar Jogo
 static async findGame(req, res) {
-    const findGame = req.body;
+    const findGame= req.body; //Body acha o jogo por nome extato
     try {
-    const findGame = await database.Games.findOne({
+    const findGameItem = await database.Game.findOne({
         where:{
-            findGame
+            name: findGame.name           
         }
     });
-    return res.status(200).menssage("jogo Deletado");
+    return res.status(200).json(findGameItem);
 } catch (error) {
-    return res.status(500).menssage("Falha ao deletar Game");
+    return res.status(500).json(error.message);
+}}
+
+//Encontrar Jogos
+
+static async findGames(req, res) {
+    const findGame= req.body;     //Body acha o jogo por nome    
+    try {
+    const findGameItem = await database.Game.findAll({
+        where:{
+            name:{[Op.like]: '%'+findGame.name +'%'   } 
+        }
+    });
+    return res.status(200).json(findGameItem);
+} catch (error) {
+    return res.status(500).json(error.message);
+}}
+
+//Encontrar Jogos por Genero
+static async findGameGenre(req, res) {
+    const findGameGenre = req.body; //Body acha o jogo por nome
+    try {
+    const findGame = await database.Game_Genre.findAll({
+        where:{
+            genre:findGameGenre.genre      
+        }
+    });
+    return res.status(200).json(findGame);
+} catch (error) {
+    return res.status(500).json(error.message);
 }
-}}*/ 
+}
+}
 
 module.exports = GameController;
 
--- src/routes/gameRoute.js
+++ src/routes/gameRoute.js
@@ -4,8 +4,11 @@ const GameController = require('../controllers/GameController');
 const router = Router();
 
 router.post('/registerGame', GameController.registerGame); // Rota de registro de novo jogo
-//router.patch('/updateGame', GameController.updateGame); // Rota para atualizar informações do jogo
+router.patch('/updateGame', GameController.updateGame); // Rota para atualizar informações do jogo
 router.delete('/deleteGame', GameController.deleteGame); // Rota de exclusão de jogo
-///router.get('/searchGame', GameController.findGame); // Rota de exclusão de jogo
+router.get('/searchGame', GameController.findGame); // Rota de Busca de Jogo
+router.get('/searchGames', GameController.findGames); // Rota de Busca de Jogos
+router.get('searchGenre', GameController.findGameGenre); // Rota de Busca de Jogo por Genero
+
 
 module.exports = router;
