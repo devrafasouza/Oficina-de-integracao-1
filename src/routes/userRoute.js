68d3c87144c853c8c79eb02d2661a43f6ee85311
Rafael de Souza Alcântara <64436420+devrafasouza@users.noreply.github.com>
2022-11-16 10:09:26 -0300

    Update src/routes/userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post('/register', UserController.registerUser); // Rota de registro de usuario
 router.post('/login', UserController.loginUser); // Rota de login de usuario
-router.post('/wallet:id', auth, UserController.purchaseUser); // Rota para desativar um usuario
+router.post('/purchase/:id_user', UserController.purchaseUser); // Rota para cadastrar uma compra e associar a um usuario
 //router.get('/wallet/:id', /*UserController.walletUser */); // Rota para acessar a carteira do usuario
 
 module.exports = router;
