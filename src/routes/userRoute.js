f4800dc9f03730561841e79982cb52de84248cc1
Rafael de Souza Alcântara <64436420+devrafasouza@users.noreply.github.com>
2022-11-17 19:58:04 -0300

    Update src/routes/userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UserController');
-const auth = require('../middlewares/auth.js');
+const auth = require('../middlewares/auth');
+const isMyId = require('../middlewares/isMyId');
 
 const router = Router();
 
 router.post('/register', UserController.registerUser); // Rota de registro de usuario
 router.post('/login', UserController.loginUser); // Rota de login de usuario
-router.post('/purchase/:id_user', UserController.purchaseUser); // Rota para cadastrar uma compra e associar a um usuario
-//router.get('/wallet/:id', /*UserController.walletUser */); // Rota para acessar a carteira do usuario
+router.post('/purchase/:id_user', auth, isMyId, UserController.purchaseUser); // Rota para cadastrar uma compra e associar a um usuario
+//router.post('/wallet/:id_user', auth, isMyId, UserController.walletUser); // *NÃO ESTÁ FUNCIONANDO* Rota para acessar a carteira do usuario
 
 module.exports = router;
