58021b71028fa20b353e0c0a7470a080eb0d21d8
MateusChicoli <mateus_chicoli@hotmail.com>
2022-11-18 17:08:09 -0300

    Update GenreServices.js

--- src/services/GenreServices.js
+++ src/services/GenreServices.js
@@ -1,5 +1,7 @@
 const Services = require('./Services.js');
 const database = require('../models');
+const { Op } = require("sequelize");
+
 
 class GenreServices extends Services {
   constructor(){
@@ -10,9 +12,33 @@ class GenreServices extends Services {
     return database[this.nameModel].create(newGenre);
 
   }
+  async getRegister(info) {
+    return database[this.nameModel].findOne({
+      where: {[Op.or]: [{id_genre: info}, {name: info}]
+      }});
+  }
 
-  async getRegister(id_genre) {
-    return database[this.nameModel].findOne({ where: { id_genre: id_genre }});
+  async getRegisterName(name) {
+    return database[this.nameModel].findAll({
+      where: { name: name
+      }});
+  }
+  async updateRegister(date,id_genre) {
+    return database[this.nameModel].update(date,{
+      where: { id_genre: id_genre
+      }});
+  }
+
+  async deleteRegister(id_genre) {
+    return database[this.nameModel].destroy({
+      where: { id_genre: id_genre
+      }});
+  }
+
+  async getAllRegister(id_genre) {
+    return database[this.nameModel].findAll({
+      where: { id_genre: id_genre
+      }});
   }
 
 }
