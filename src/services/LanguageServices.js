c03b48135f09dc41a2ef1cc1f415d0de4041c5b1
MateusChicoli <mateus_chicoli@hotmail.com>
2022-11-18 17:34:56 -0300

    Update LanguageServices.js

--- src/services/LanguageServices.js
+++ src/services/LanguageServices.js
@@ -1,5 +1,7 @@
 const Services = require('./Services.js');
 const database = require('../models');
+const { Op } = require("sequelize");
+
 
 class LanguageServices extends Services {
   constructor(){
@@ -10,9 +12,33 @@ class LanguageServices extends Services {
     return database[this.nameModel].create(newLanguage);
 
   }
+  async getRegister(info) {
+    return database[this.nameModel].findOne({
+      where: {[Op.or]: [{id_Language: info}, {name: info}]
+      }});
+  }
 
-  async getRegister(id_language) {
-    return database[this.nameModel].findOne({ where: { id_language: id_language }});
+  async getRegisterName(name) {
+    return database[this.nameModel].findAll({
+      where: { name: name
+      }});
+  }
+  async updateRegister(date,id_language) {
+    return database[this.nameModel].update(date,{
+      where: { id_language: id_language
+      }});
+  }
+
+  async deleteRegister(id_language) {
+    return database[this.nameModel].destroy({
+      where: { id_language: id_language
+      }});
+  }
+
+  async getAllRegister(id_language) {
+    return database[this.nameModel].findAll({
+      where: { id_language: id_language
+      }});
   }
 
 }
