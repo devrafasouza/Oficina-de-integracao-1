bcc403699001d506b3c9390bed6be8832b8ac31d
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-12-03 20:41:12 -0300

    1

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
 module.exports = async (req, res, next) => {
-    if(req.user.isAdmin == 1) {
+    if(req.user.type == 'admin') {
       next();
     } else {
       return res.status(401).json({ message: "Você não tem autorização para essa requisição" });
