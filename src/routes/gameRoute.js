77ef3bdd62babcf674045d6219f3f5f67e520cca
Fijorador <108754679+Fijorador@users.noreply.github.com>
2022-12-04 17:40:47 -0300

    1

--- src/routes/gameRoute.js
+++ src/routes/gameRoute.js
@@ -1,23 +1,18 @@
 const { Router } = require('express');
+const auth = require('../middlewares/auth');
+const isMyId = require('../middlewares/isMyId');
+const isActive = require('../middlewares/isActive.js');
+const isAdmin = require('../middlewares/isAdmin.js');
 const GameController = require('../controllers/GameController');
-const Game_GenreController = require('../controllers/Game_GenreController');
 
 
 const router = Router();
 
-router.post('/registerGame', GameController.registerGame); // Rota de registro de novo jogo
-
-router.patch('/updateGame', GameController.updateGame); // Rota para atualizar informações do jogo
+router.post('/registerGame:id_user', isActive, auth, isMyId,isAdmin, GameController.registerGame); // Rota de registro de novo jogo
+router.patch('/updateGame:id_user', isActive, auth, isMyId,isAdmin, GameController.updateGame); // Rota para atualizar informações do jogo
 router.get('/searchGamesName', GameController.searchGamesName); // Rota de Busca de Jogos
 router.get('/searchGamesId', GameController.searchGamesId); // Rota de Busca de Jogo #Id
 
-//
-router.post('/registerGame/:id_game/Genre', Game_GenreController.registerGameGenre); // Rota de registro de novo jogo
-//
-router.get('/searchGamesCross', GameController.searchGamesCross); // Rota de Busca de Jogos
-
-
-router.delete('/deleteGame', GameController.deleteGame); // Rota de exclusão de jogo
 
 
 
--- src/routes/genreRoute.js
+++ src/routes/genreRoute.js
@@ -5,8 +5,8 @@ const router = Router();
 
 router.post('/registerGenre', GenreController.registerGenre); // Rota de registro de novo jogo
 router.patch('/updateGenre', GenreController.updateGenre); // Rota para atualizar informações do jogo
-router.get('/searchGenreName', GenreController.searchGenreName); // Rota de Busca de Jogos
-router.get('/searchGenreId', GenreController.searchGenreId);
+router.get('/searchGenreName', GenreController.searchGenreName); // Rota de Busca de Generos Name
+router.get('/searchGenreId', GenreController.searchGenreId); // Rota de Busca de Generos ID
 router.delete('/deleteGenre', GenreController.deleteGenre); // Rota de exclusão de jogo*/
 
 
